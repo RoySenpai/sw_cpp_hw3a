3af72202497e7f013e8612158f56603398c35e73
Roy <me@hiyorix.com>
2023-04-14 16:18:36 +0300

    Update test

--- Test.cpp
+++ Test.cpp
@@ -6,10 +6,17 @@
 using namespace std;
 using namespace ariel;
 
-TEST_CASE("Fraction constructor throws exception when denominator is zero")
+TEST_CASE("Test 1: Fraction constructor throws exception when denominator is zero")
 {
+    // The following line should throw an exception of type "invalid_argument"
     CHECK_THROWS(Fraction(1, 0));
+
+    // This is a valid fraction (1/-1) = -1
     CHECK_NOTHROW(Fraction(1, -1));
+
+    // The following line should throw an exception of type "invalid_argument"
     CHECK_THROWS(Fraction(-1, 0));
+
+    // This is a valid fraction (-1/-1) = 1
     CHECK_NOTHROW(Fraction(-1, -1));
 }
